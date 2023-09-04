2630e3de1319b2bdbf35d5a5e623bd2ed5e098c3
Cory <cameyer182@gmail.com>
2023-09-03 20:23:11 -0400

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,40 @@
+import { tours } from '../data'
+import Title from './Title'
+
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <Title title="Featured" subTitle="Tours" />
+
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          return (
+            <article key={tour.id} className="tour-card">
+              <div className="tour-img-container">
+                <img src={tour.img} className="tour-img" alt="" />
+                <p className="tour-date">{tour.date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{tour.title}</h4>
+                </div>
+                <p>{tour.info}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>
+                    {tour.location}
+                  </p>
+                  <p>{tour.length}</p>
+                  <p>{tour.cost}</p>
+                </div>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+export default Tours
